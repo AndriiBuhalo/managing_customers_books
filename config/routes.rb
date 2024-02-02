a954a9c16d5cf9253c9a52db71a236d7f3194a6e
andrii.bukhalo <andrii.bukhalo@cd.p2h.com>
2024-02-02 12:20:52 +0200

    setup sessions and registrations controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
+  devise_for :customers, path: '', path_names: {
+    sign_in: 'login',
+    sign_out: 'logout',
+    registration: 'signup'
+  },
+  controllers: {
+    sessions: 'customers/sessions',
+    registrations: 'customers/registrations'
+  }
+
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
